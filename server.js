b2448eac8bd03aec749fe8abfb426668fe40e5a3
Akhil968-web <quizcrackers28@gmail.com>
2025-09-21 08:59:00 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
+let clients = [];
+
+wss.on('connection', (ws) => {
+  console.log('New client connected');
+  clients.push(ws);
+
+  ws.on('message', (message) => {
+    clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    clients = clients.filter(c => c !== ws);
+    console.log('Client disconnected');
+  });
+});
+
+console.log('WebSocket server running');
